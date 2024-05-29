a42976cc12d561755d9cd49fcc8df3c7a6d4a2aa
Lael Shin <147151153+laelshin@users.noreply.github.com>
2024-05-29 16:26:13 +0900

    Update mgs_code.py
    Changed Dmin and Dist, Considered a periodic condition

--- Mulguisin/mgs_code.py
+++ Mulguisin/mgs_code.py
@@ -2,7 +2,11 @@ from numba import njit
 from numba.typed import List
 
 @njit
+@njit
 def mgs3d(Rcut,x1,y1,z1,isort):
+    
+	t_start = time()
+    
 	Nmgs=0
 	imgs = List()
 	cng = List()
@@ -13,10 +17,11 @@ def mgs3d(Rcut,x1,y1,z1,isort):
 		ic = isort[i]
 		Icc=-1
 		Iwg=-1
-		Dmin=999
+		# Dmin larger than Rcut**2
+		Dmin=Rcut**2 + 1
 
 		for ig in range(0,Nmgs):
-			Dist=999
+			Dist=Rcut**2 + 1
 			for icl in range(0,cng[ig]):
 				itmp=clg[ig][icl]
 				d2 = (x1[ic]-x1[itmp])**2+(y1[ic]-y1[itmp])**2+(z1[ic]-z1[itmp])**2
@@ -38,10 +43,17 @@ def mgs3d(Rcut,x1,y1,z1,isort):
 			clm.append([ic])
 			cng.append(1)
 			Nmgs+=1
+    
+	t_end = time()
+	print('Calculation is done. Time = ',t_end - t_start)
+    
 	return Nmgs,imgs,clg,clm,cng
 
 @njit
-def mgs2d(Rcut,x1,y1,isort):
+def mgs3d_period(Rcut,x1,y1,z1,isort,Lbox):
+    
+#	t_start = time()
+    
 	Nmgs=0
 	imgs = List()
 	cng = List()
@@ -52,13 +64,29 @@ def mgs2d(Rcut,x1,y1,isort):
 		ic = isort[i]
 		Icc=-1
 		Iwg=-1
-		Dmin=999
+		# Dmin larger than Rcut**2
+		Dmin=Rcut**2 + 1
 
 		for ig in range(0,Nmgs):
-			Dist=999
+			Dist=Rcut**2 + 1
 			for icl in range(0,cng[ig]):
 				itmp=clg[ig][icl]
-				d2 = (x1[ic]-x1[itmp])**2+(y1[ic]-y1[itmp])**2
+                		#We are calculating the distance, so only the absolute value is necessary.
+				del_x = np.abs(x1[ic]-x1[itmp])
+				del_y = np.abs(y1[ic]-y1[itmp])
+				del_z = np.abs(z1[ic]-z1[itmp])
+                
+                		#Periodic boundary condition. We assume that the cluster size is smaller than 0.5*Lbox
+				if del_x > Lbox*0.5:
+				    del_x = Lbox - del_x
+				if del_y > Lbox*0.5:
+				    del_y = Lbox - del_y
+				if del_z > Lbox*0.5:
+				    del_z = Lbox - del_z
+    
+                
+				d2 = (del_x)**2+(del_y)**2+(del_z)**2                
+        
 				if (d2<Dist): 
 					idmin=itmp
 					Dist=d2
